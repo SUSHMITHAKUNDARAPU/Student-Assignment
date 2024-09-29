07f6725256fc404380c8e5683c70c42a5f110d72
unknown <ajaymedidhi858@gmail.com>
2024-09-29 20:15:15 +0530

    initial commit

---
+++ venv/models.py
@@ -0,0 +1,16 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    grade = db.Column(db.Float, nullable=True)  # Add this line for grade storage
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
